4701c279c69d9a3eb60a7c1df9891f8b076fb6d6
Dr. Christoph 'Schorsch' Jung <c-jung@t-systems.com>
2022-03-27 17:18:34 +0200

    https://jira.catena-x.net/browse/ART3-305 There seems to be a prob with Feign->Wrapper and the Bearer Token

--- eclipsedataspaceconnector/api-wrapper/4-aas-submodel-post.sh
+++ eclipsedataspaceconnector/api-wrapper/4-aas-submodel-post.sh
@@ -6,7 +6,7 @@ set -o pipefail
 set -o nounset
 
 curl -X POST \
-     "http://localhost:8193/api/service/offer-orga/shells/398c047f-c14c-47b9-9c27-d9511afc90d3/aas/idconversion-aspect/submodel/submodel-elements/anonymize/invoke?content=value&async=false&provider-connector-url=http://provider-control-plane:9191" \
+     "http://localhost:8193/api/service/offer-orga/shells/398c047f-c14c-47b9-9c27-d9511afc90d3/aas/idconversion-aspect/submodel/submodel-elements/convert/invoke?content=value&async=false&provider-connector-url=http://provider-control-plane:9191" \
      --header "Authorization: Basic c29tZXVzZXI6c29tZXBhc3N3b3Jk" \
      --header 'Content-Type: application/json' \
      --data-raw '{
