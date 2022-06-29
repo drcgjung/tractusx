8aeaef76cc3fdc95f73cd49a9fbf16c8d17f26c5
Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>
2022-06-29 17:20:16 +0200

    https://jira.catena-x.net/browse/A1SLDT-627 yeah. add lot skill

--- portal/code/tractus-x-portal/src/components/knowledgeagent/data.ts
+++ portal/code/tractus-x-portal/src/components/knowledgeagent/data.ts
@@ -106,25 +106,31 @@ const traceSkill : Skill = {
         let query=`
 PREFIX cx: <https://github.com/eclipse/tractusx/>
 PREFIX qudt: <http://qudt.org/schema/qudt/>
+
 SELECT DISTINCT ?vehicle ?material
 WHERE {
     BIND("${material}" as ?material).
 
-   # From the SerializedPart of tenant2 we filter a specific Material
+    # Connect to Tenant2=Supplier Data Plane
     SERVICE <${process.env.REACT_APP_TENANT2}> {
+
+        # Inject "Riddle" into the SerializedPart Asset
         GRAPH <urn:cx:graph#serializedPart> {
+
             ?material_type cx:hasPartTypeName ?material .
-            ?material_component a cx:SerializedPart ;
+            ?material_component a cx:SerializedPart;
                 cx:belongsTo ?material_type .
-            ?vehicle a cx:SerializedPart.
-            ?vehicle cx:belongsTo ?specific_vehicle_type.
+            ?vehicle a cx:SerializedPart;
+                cx:belongsTo ?specific_vehicle_type.
             ?specific_vehicle_type cx:hasPartTypeName "Vehicle Model A" .
         }
     }
 
-    # We need the BOM of the vehicles which are provided by Tenant1
-    SERVICE <${process.env.REACT_APP_TENANT1}> {                      # Connector Data Plane of Tenant1
-        GRAPH <urn:cx:graph#assemblyPartRelation> {                   # Graph asset of Tenant2
+    # Connect to Tenant1=OEM Data Plane
+    SERVICE <${process.env.REACT_APP_TENANT1}> {
+        # Inject "Riddle" into the Assembly Asset
+        GRAPH <urn:cx:graph#assemblyPartRelation> {
+            # Transitive closure
             ?vehicle cx:consistsOf* ?material_component .
         }
     }
@@ -140,6 +146,76 @@ WHERE {
     return {
          phrases:[`Hey Schorsch`, `${material}`, `is contained in`,res.results.bindings.length.toString(),"vehicles"],
          pauses:[0,10,5,5,0]
+    };
+  }
+};
+
+/**
+ * Example of a skill which lists vehicles
+ * containing a certain component
+ */
+const lotQuestionExp : RegExp = /^.*affected by lot ([0-9\-A-Za-z ]+).*$/i;
+
+const lotSkill : Skill = {
+  nick: "lot",
+  name: "Lot Skill (FOSS)",
+
+  match: function(utterance:string) {
+     if(lotQuestionExp.test(utterance)) {
+        let match=utterance.match(lotQuestionExp)
+        let lotNumber=match[1];
+        let query=`
+PREFIX cx: <https://github.com/eclipse/tractusx/>
+SELECT ?lotNumber ?lotMaterial ?aggregate ?aggregateType
+WHERE {
+
+    BIND("${lotNumber}" as ?lotNumberPrefix)
+
+    # We're looking for a particular component
+    SERVICE <${process.env.REACT_APP_TENANT2}> {
+        GRAPH <urn:cx:graph#serializedPart> {
+            ?lot cx:hasPartTypeNumber ?lotNumber.
+            ?lot cx:hasPartTypeName ?lotMaterial.
+            ?component cx:belongsTo ?lot.
+        }
+    }
+
+    FILTER regex(str(?lotNumber), ?lotNumberPrefix).
+
+    # We need the BOM  of Tenant1 to trace it through
+    SERVICE <${process.env.REACT_APP_TENANT1}> {
+        GRAPH <urn:cx:graph#assemblyPartRelation> {
+            ?aggregate cx:consistsOf+ ?component .
+        }
+    }
+
+    # We're looking for a particular component
+    SERVICE <${process.env.REACT_APP_TENANT2}> {
+        GRAPH <urn:cx:graph#serializedPart> {
+            ?aggregateType cx:hasPartTypeNumber ?aggregateNumber.
+            ?aggregate cx:belongsTo ?aggregateType.
+        }
+    }
+}`;
+        return query;
+     } else {
+        return null;
+     }
+  },
+
+  summarize: function(res:SparqlResult) {
+    var lot = res.results.bindings[0]["lotNumber"]["value"];
+    var lotMaterial = res.results.bindings[0]["lotMaterial"]["value"];
+    var noVehicles = 0;
+    res.results.bindings.forEach( function(binding) {
+        var aggTypeName : String = binding["aggregateType"]["value"].toString();
+        if(aggTypeName.includes("Vehicle")) {
+            noVehicles=noVehicles+1;
+        }
+    });
+    return {
+         phrases:[`Hey Schorsch`, `die Charge ${lot} enthält ${lotMaterial}`, `und betrifft`,res.results.bindings.length.toString(),"aggregate in",noVehicles.toString(),"fahrzeugen."],
+         pauses:[0,0,0,0,0,0,0]
     };
   }
 };
@@ -161,6 +237,7 @@ export const unknownSkill : Skill = {
 export const skills:Skill[] = [
  materialSkill,
  traceSkill,
+ lotSkill,
  unknownSkill
 ];
 
